2e391d7cede9710c2e7b61b0172b163bb27cec3b
Howu <j6y3cl6@hotmail.com>
2018-11-05 16:56:51 +0800

    fix product_manage

--- admin/cls/Product_Manage.cls.php
+++ admin/cls/Product_Manage.cls.php
@@ -557,16 +557,10 @@
 			date_default_timezone_set('Asia/Taipei');
 			$date = date('Y-m-d H:i:s', time());
 			foreach($array as $key =>$value){
-<<<<<<< HEAD
-				if(gettype($value) == "string"){
-					$$key = mysqli_real_escape_string($this->db->oDbLink, $value);
-				}
-=======
                 if(gettype($value) == "string"){
                     $$key = mysqli_real_escape_string($this->db->oDbLink, $value);
                 }
 //				$$key = mysqli_real_escape_string($this->db->oDbLink, $value);
->>>>>>> ronghong
 			}
 			//若要統一更改上架狀態
 			$sql = "update
