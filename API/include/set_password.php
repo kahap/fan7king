d4b7f4485ba51dfce5b96e346785ef3e89ae07b0
Howu <j6y3cl6@hotmail.com>
2018-11-09 16:09:07 +0800

    fix API password

--- API/include/set_password.php
+++ API/include/set_password.php
@@ -5,38 +5,32 @@
     foreach ($_POST as $key => $value) {
         $$key = $value;
     }
-    
-    if (isset($token)) {
-        $app_data = new API("app_data");
-        $app_data->setWhereArray(array("adTokenId"=>$token));    
-        $app_data->getWithWhereAndJoinClause();
-        $apData = $app_data->getData();
-        $memNo = $apData[0]["memNo"];
-        if ($memNo=="") {
-            $api->setInformation(FALSE, 0, 0, "搜尋失敗");
+    $app_data = new API("app_data");
+    $app_data->setWhereArray(array("adTokenId"=>$token));    
+    $app_data->getWithWhereAndJoinClause();
+    $apData = $app_data->getData();
+    $memNo = $apData[0]["memNo"];
+
+    if ($type="regist") {        
+        $sql = "UPDATE member SET memPwd='".$password."' , memCell='".$memCell."' WHERE memNo='".$memNo."' ";
+        $api->customSql($sql);
+        $api->setInformation(TRUE, 1, 1, "成功修改！");
+    }else if($type=="forget"){
+        $sql = "UPDATE member SET memPwd='".$password."' WHERE memCell='".$memCell."' ";
+        $api->customSql($sql);
+        $api->setInformation(TRUE, 1, 1, "成功修改！");
+    }else if($type=="change"){        
+        $api->setWhereArray(array("memNo"=>$memNo,"memPwd"=>$passwordOld));    
+        $api->getWithWhereAndJoinClause();
+        $result = $api->getData();
+        if (count($result)==0) {
+            $api->setInformation(FALSE, 1, 1, "原密碼錯誤");
         }else{
-            $addsql="";
-            if (isset($memClass) && $memClass!="") {
-                $addsql=", memClass='".$memClass."' ";
-            }
-            $sql = "UPDATE member SET memPwd='".$password."' ".$addsql." WHERE memNo='".$memNo."' ";
-            $api->customSql($sql);
-            $api->setInformation(TRUE, 1, 1, "成功修改！");    
-        }
-    }else{
-        $forSave = new API("member");
-        $forSave->setWhereArray(array("memCell"=>$_POST["memCell"]));
-        $forSave->getWithWhereAndJoinClause();
-        $apData = $forSave->getData();
-        $memNo = $apData[0]["memNo"];
-        if ($memNo=="") {
-            $api->setInformation(FALSE, 0, 0, "搜尋失敗");            
-        }else{            
-            $sql = "UPDATE member SET memPwd='".$password."' WHERE memCell='".$memCell."' ";
+            $sql = "UPDATE member SET memPwd='".$password."' WHERE memNo='".$memNo."' and  memPwd='".$passwordOld."' ";
             $api->customSql($sql);
             $api->setInformation(TRUE, 1, 1, "成功修改！");
         }        
-    }    
+    }
     //返回修改狀態
     $api->setResult();
 ?>
