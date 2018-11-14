6bd8b65626e5a09f827b3f5313895134112c00b7
Howu <j6y3cl6@hotmail.com>
2018-11-14 17:32:52 +0800

    FB Test

--- portal/Controllers/fb-php/sharcode.php
+++ portal/Controllers/fb-php/sharcode.php
@@ -4,9 +4,9 @@ session_start();
 require_once 'src/Facebook/autoload.php';
 
 $helper = $fb->getRedirectLoginHelper();
-$permissions = ['email','user_friends','public_profile', 'user_birthday','user_location','user_link'];
+// $permissions = ['email','user_friends','public_profile', 'user_birthday','user_location','user_link'];
 //friend後面那些，需要詢問權限
-// $permissions = ['email'];
+$permissions = ['email'];
 $loginUrl = $helper->getLoginUrl('http://nowait.kahap.com/fan7king_dev2/fb-php/fbconfig.php', $permissions);
 	$_SESSION['user']['sharcode'] = $_GET['sharcode'];
 	header('Location: '.$loginUrl);
