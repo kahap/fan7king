abdc1ecbf2272d4398cdfb910333067cd72a540e
Howu <j6y3cl6@hotmail.com>
2018-11-15 09:22:00 +0800

    change 有效的 OAuth 重新導向 URI

--- portal/Controllers/fb-php/sharcode.php
+++ portal/Controllers/fb-php/sharcode.php
@@ -7,7 +7,7 @@ $helper = $fb->getRedirectLoginHelper();
 // $permissions = ['email','user_friends','public_profile', 'user_birthday','user_location','user_link'];
 //friend後面那些，需要詢問權限
 $permissions = ['email'];
-$loginUrl = $helper->getLoginUrl('http://nowait.kahap.com/fan7king_dev2/fb-php/fbconfig.php', $permissions);
+$loginUrl = $helper->getLoginUrl('https://test.nowait.shop/fb-php/fbconfig.php', $permissions);
 	$_SESSION['user']['sharcode'] = $_GET['sharcode'];
 	header('Location: '.$loginUrl);
 
