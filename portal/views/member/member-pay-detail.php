dc96a10ac8aa7636bf43f17f8e095f23167bdadb
ronghong_kahap <ronghong@kahap.com>
2018-11-21 17:11:47 +0800

    new portal add pay_detail function

--- portal/views/member/member-pay-detail.php
+++ portal/views/member/member-pay-detail.php
@@ -1,3 +1,30 @@
+<?php
+$member = new Member();
+$memberData = $member->getOneMemberByNo($_SESSION['user']['memNo']);
+$memOrigData = $member->getOneMemberByNo($_SESSION['user']['memNo']);
+$member->changeToReadable($memberData[0]);
+
+$or = new Orders();
+$orMemData = $or->getOrByMemberAndMethod($_SESSION['user']['memNo'],1, 0,9999);
+
+$pm = new Product_Manage();
+$pro = new Product();
+
+//訂單
+$orNo = $_GET["orno"];
+$orOrigData = $or->getOneOrderByNo($orNo);
+$orData = $or->getOneOrderByNo($orNo);
+$or->changeToReadable($orData[0],$method);
+
+//欄位名稱
+$columnName = $or->getAllColumnNames("orders");
+
+//商品上架
+$pmData = $pm->getOnePMByNo($orData[0]["pmNo"]);
+
+//商品
+$proData = $pro->getOneProByNo($pmData[0]["proNo"]);
+?>
 
 <main role="main">
     <h1><span>會員中心</span><small>member center</small></h1>
@@ -5,14 +32,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-3">
-                    <div class="list-group">
-                        <a href="member-info.htm" class="list-group-item list-group-item-action">基本資料</a>
-                        <a href="member-change.htm" class="list-group-item list-group-item-action">變更密碼</a>
-                        <a href="member-order.htm" class="list-group-item list-group-item-action">訂單查詢</a>
-                        <a href="member-pay.htm" class="list-group-item list-group-item-action active">我要繳款</a>
-                    </div>
-                    <div class="sell xs-none" style="height: 430px;background-image: linear-gradient(151deg, #ff7f00,#fff0c9);">
-                    </div>
+                    <?php
+                    $active = 3;
+                    require_once 'portal/views/member/_left.php';
+                    ?>
                 </div>
                 <div class="col-lg-9">
                     <div class="section-inner-table bg-white">
@@ -27,79 +50,56 @@
                                 <th nowrap="nowrap">繳款方式</th>
                             </tr>
                             </thead>
-                            <tbody>
-                            <tr>
-                                <td data-th="期數" nowrap="nowrap" class="text-center">6</td>
-                                <td data-th="應繳款日" nowrap="nowrap">2018-09-21</td>
-                                <td data-th="實際繳款日"></td>
-                                <td data-th="應繳金額" nowrap="nowrap">1000</td>
-                                <td data-th="還款金額" nowrap="nowrap"></td>
-                                <td data-th="繳款方式" class="payment" nowrap="nowrap">
-                                    <a href="#" title="網銀/ATM匯入繳款" data-toggle="modal" data-target="#exampleModal"><img src="images/icon-atm.png" alt="網銀/ATM匯入繳款"></a>
-                                    <a href="#" title="全家、萊爾富超商掃描電子條碼付款"><img src="images/icon-barcode.png" alt="全家、萊爾富超商掃描電子條碼付款"></a>
-                                    <a href="#" title="列印紙本四大超商付款"><img src="images/icon-printer.png" alt="列印紙本四大超商付款"></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td data-th="期數" nowrap="nowrap" class="text-center">5</td>
-                                <td data-th="應繳款日" nowrap="nowrap">2018-09-21</td>
-                                <td data-th="實際繳款日"></td>
-                                <td data-th="應繳金額" nowrap="nowrap">1000</td>
-                                <td data-th="還款金額" nowrap="nowrap"></td>
-                                <td data-th="繳款方式" class="payment" nowrap="nowrap">
-                                    <a href="#" title="網銀/ATM匯入繳款" data-toggle="modal" data-target="#exampleModal"><img src="images/icon-atm.png" alt="網銀/ATM匯入繳款"></a>
-                                    <a href="#" title="全家、萊爾富超商掃描電子條碼付款"><img src="images/icon-barcode.png" alt="全家、萊爾富超商掃描電子條碼付款"></a>
-                                    <a href="#" title="列印紙本四大超商付款"><img src="images/icon-printer.png" alt="列印紙本四大超商付款"></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td data-th="期數" nowrap="nowrap" class="text-center">4</td>
-                                <td data-th="應繳款日" nowrap="nowrap">2018-09-21</td>
-                                <td data-th="實際繳款日"></td>
-                                <td data-th="應繳金額" nowrap="nowrap">1000</td>
-                                <td data-th="還款金額" nowrap="nowrap"></td>
-                                <td data-th="繳款方式" class="payment" nowrap="nowrap">
-                                    <a href="#" title="網銀/ATM匯入繳款" data-toggle="modal" data-target="#exampleModal"><img src="images/icon-atm.png" alt="網銀/ATM匯入繳款"></a>
-                                    <a href="#" title="全家、萊爾富超商掃描電子條碼付款"><img src="images/icon-barcode.png" alt="全家、萊爾富超商掃描電子條碼付款"></a>
-                                    <a href="#" title="列印紙本四大超商付款"><img src="images/icon-printer.png" alt="列印紙本四大超商付款"></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td data-th="期數" nowrap="nowrap" class="text-center">3</td>
-                                <td data-th="應繳款日" nowrap="nowrap">2018-09-21</td>
-                                <td data-th="實際繳款日"></td>
-                                <td data-th="應繳金額" nowrap="nowrap">1000</td>
-                                <td data-th="還款金額" nowrap="nowrap"></td>
-                                <td data-th="繳款方式" class="payment" nowrap="nowrap">
-                                    <a href="#" title="網銀/ATM匯入繳款" data-toggle="modal" data-target="#exampleModal"><img src="images/icon-atm.png" alt="網銀/ATM匯入繳款"></a>
-                                    <a href="#" title="全家、萊爾富超商掃描電子條碼付款"><img src="images/icon-barcode.png" alt="全家、萊爾富超商掃描電子條碼付款"></a>
-                                    <a href="#" title="列印紙本四大超商付款"><img src="images/icon-printer.png" alt="列印紙本四大超商付款"></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td data-th="期數" nowrap="nowrap" class="text-center">2</td>
-                                <td data-th="應繳款日" nowrap="nowrap">2018-09-21</td>
-                                <td data-th="實際繳款日"></td>
-                                <td data-th="應繳金額" nowrap="nowrap">1000</td>
-                                <td data-th="還款金額" nowrap="nowrap"></td>
-                                <td data-th="繳款方式" class="payment" nowrap="nowrap">
-                                    <a href="#" title="網銀/ATM匯入繳款" data-toggle="modal" data-target="#exampleModal"><img src="images/icon-atm.png" alt="網銀/ATM匯入繳款"></a>
-                                    <a href="#" title="全家、萊爾富超商掃描電子條碼付款"><img src="images/icon-barcode.png" alt="全家、萊爾富超商掃描電子條碼付款"></a>
-                                    <a href="#" title="列印紙本四大超商付款"><img src="images/icon-printer.png" alt="列印紙本四大超商付款"></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td data-th="期數" nowrap="nowrap" class="text-center">1</td>
-                                <td data-th="應繳款日" nowrap="nowrap">2018-09-21</td>
-                                <td data-th="實際繳款日"></td>
-                                <td data-th="應繳金額" nowrap="nowrap">1000</td>
-                                <td data-th="還款金額" nowrap="nowrap"></td>
-                                <td data-th="繳款方式" class="payment" nowrap="nowrap">
-                                    <a href="#" title="網銀/ATM匯入繳款" data-toggle="modal" data-target="#exampleModal"><img src="images/icon-atm.png" alt="網銀/ATM匯入繳款"></a>
-                                    <a href="#" title="全家、萊爾富超商掃描電子條碼付款"><img src="images/icon-barcode.png" alt="全家、萊爾富超商掃描電子條碼付款"></a>
-                                    <a href="#" title="列印紙本四大超商付款"><img src="images/icon-printer.png" alt="列印紙本四大超商付款"></a>
-                                </td>
-                            </tr>
+                            <?php
+                            if($orMemData != null){
+                                foreach($orMemData as $key=>$value){
+
+                                    $orig = $value;
+                                    $or->changeToReadable($value,$value["orMethod"]);
+                                    $pmData = $pm->getOnePMByNo($value["pmNo"]);
+                                    $proData = $pro->getOneProByNo($pmData[0]["proNo"]);
+                                    if($value["orStatus"] == '我要繳款'){
+                                        ?>
+                                        <tbody>
+                                        <tr>
+                                            <td data-th="期數" nowrap="nowrap" class="text-center"><?php echo $value["orPeriodAmnt"];?></td>
+                                            <td data-th="應繳款日" nowrap="nowrap"><?php echo $value["orHandlePaySupDate"];?></td>
+                                            <td data-th="實際繳款日"><?php echo $value["orHandlePaySupDate"];?></td>
+                                            <td data-th="應繳金額" nowrap="nowrap"><?php echo $value["orPeriodTotal"]/$value['orPeriodAmnt'];?></td>
+                                            <td data-th="還款金額" nowrap="nowrap"></td>
+                                            <td data-th="繳款方式" class="payment" nowrap="nowrap">
+                                            <?php
+                                            if ($value["prPayBy"]==0){
+                                                ?>
+                                                <a href="#" title="網銀/ATM匯入繳款" data-toggle="modal" data-target="#exampleModal">
+                                                    <img src="images/icon-atm.png" alt="網銀/ATM匯入繳款">
+                                                </a>
+                                                <?php
+                                            }elseif ($value["prPayBy"]==1) {
+                                                ?>
+                                                <a href="#" title="全家、萊爾富超商掃描電子條碼付款">
+                                                    <img src="images/icon-barcode.png" alt="全家、萊爾富超商掃描電子條碼付款">
+                                                </a>
+                                                <?php
+                                            }elseif ($value["prPayBy"]==2){
+                                                ?>
+                                                <a href="#" title="列印紙本四大超商付款">
+                                                    <img src="images/icon-printer.png" alt="列印紙本四大超商付款">
+                                                </a>
+                                                <?php
+                                            }
+                                            ?>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                    }
+                                }
+                            }else{
+                                ?>
+                                <td colspan="5">沒有任何資料</td>
+                                <?php
+                            }
+                            ?>
                             </tbody>
                         </table>
                     </div>
